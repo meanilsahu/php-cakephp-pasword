709ec9f8ee9948bad01be528fc26a0179838b2ee
Anil Sahu <68774494+meanilsahu@users.noreply.github.com>
2024-05-16 22:50:15 +0530

    Update ccakephpp.php

--- ccakephpp.php
+++ ccakephpp.php
@@ -1,7 +1,15 @@
 <?php
 
 
-echo "cakephppasswor";
+echo " Create cakephp Passwor". ''."<br>";
+
+$defaultConfig = [
+					'hashType' => PASSWORD_DEFAULT,
+					'hashOptions' => []
+					];
+					
+				$PasswordHash=password_hash('Anilsahu@#world', $defaultConfig['hashType'] ,$defaultConfig['hashOptions']);
+				echo $PasswordHash;
 
 
 ?>
